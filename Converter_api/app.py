85bc20121c0296ecac11c3213ee7205489e56cd5
Artem <limpsfan@gmail.com>
2018-10-24 22:57:35 +0200

    caching_added

--- Converter_api/app.py
+++ Converter_api/app.py
@@ -1,54 +1,83 @@
 from flask import Flask, Request, abort, jsonify
 from flask_restful import Resource, Api, reqparse
+from flask_caching import Cache
 import json
 import requests
 
 
 def create_converter():
     app = Flask(__name__)
+    app.config.from_pyfile("D:\\Sources\\Converter\\config\\settings.py")
+    app
 
     parser = reqparse.RequestParser()
     parser.add_argument("amount", type=float, required=True, location="args")
     parser.add_argument("input_currency", required=True, location="args")
     parser.add_argument("output_currency", location="args")
 
+    cache = Cache(app, config={"CACHE_TYPE": "simple"})
+
     class Converter(Resource):
+        """Recives params from Query string and returns json output with converted data """
         def get(self):
-            args = parser.parse_args()
-            amount = args["amount"]
-            inp_cur = args["input_currency"]
-            out_cur = args["output_currency"]
+            """
+            :param amount: amount to convert
+            :type amount: float""" 
+            self._get_data()
             #get rates from API
-            data = {"base":inp_cur}
+            data = {"base":self.inp_cur}
+            rates = self._get_rates(data)
+            #convert currency
+            if self.out_cur:
+                try:
+                    rt = rates[self.out_cur]
+                    conv_amount = (self.amount * rt)
+                    output = {self.out_cur:conv_amount}
+                except KeyError:
+                    output = rates
+            else:
+                for currency in rates:
+                    rates[currency] = (self.amount * rates[currency])
+                output = rates
+
+            json_tamplate = {"input":{"amount":self.amount, "currency":self.inp_cur}, "output": output}
+            return jsonify(json_tamplate)
+
+        @staticmethod
+        @cache.memoize(timeout=300)
+        def _get_rates(data):
             try:
                 resp = requests.get(app.config["RATES_API"], params=data)
+                print("sending request")
             except Exception:
                 abort(500)
             if resp.status_code == 200:
                 rates = resp.json()["rates"]
             else:
-                abort(resp.status_code, resp.text)
-            #convert currency
+                abort(jsonify(resp.json()))
+            return(rates)
+        
+        
+        def _get_data(self):
+            """Convert symbols into currency codes """ 
+            args = parser.parse_args()
+            self.amount = args["amount"]
+            self.inp_cur = args["input_currency"]
+            self.out_cur = args["output_currency"]
 
-            if out_cur:
-                try:
-                    rt = rates[out_cur]
-                    conv_amount = (amount * rt)
-                    output = {out_cur:conv_amount}
-                except KeyError:
-                    output = rates
-            else:
-                for currency in rates:
-                    rates[currency] = (amount * rates[currency])
-                output = rates
+            fl = open(app.config["SYMBOLS_URL"], encoding="utf_8")
+            data = json.load(fl)
+            for code in data:
+                values = data[code]
+                if self.inp_cur == values["symbol"]:
+                    self.inp_cur = code
+                elif self.out_cur == values["symbol"]:
+                    self.out_cur = code
 
-            json_tamplate = {"input":{"amount":amount, "currency":inp_cur}, "output": output}
-            return jsonify(json_tamplate)
 
     api = Api(app)
     api.add_resource(Converter, '/currency_converter')
 
-    app.config.from_pyfile("D:\\Sources\\Converter\\config\\settings.py")
     return app
 
 
